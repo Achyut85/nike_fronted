6281730e36bc964d06d343cfed1f45daa606adc4
Achyut Agrahari <achyutagrahari@gmail.com>
2024-08-24 22:24:33 +0530

    intial-commit

---
+++ src/Sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+export {
+    Hero,
+    Footer,
+    PopularProduct,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Services
+}
